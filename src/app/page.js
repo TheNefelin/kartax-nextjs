c7d0dcb27ed0792a4e45d6d9b6d5e66644ff3f95
TheNefelin <metalflap@gmail.com>
2023-07-24 21:22:07 -0400

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
-import Image from 'next/image'
+import Link from 'next/link'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main>
       HOLA
+      <Link href={"https://www.google.cl"} target='_blanck' className={styles.link}>CLICK</Link>
     </main>
   )
 }
