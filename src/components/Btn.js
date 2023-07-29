3e4478df3bb6336f0a6174a24ca52299a37ce4ad
Francisco Carmona <flcarmonac@yahoo.com>
2023-07-28 21:55:35 -0400

    update

--- src/components/Btn.js
+++ src/components/Btn.js
@@ -21,7 +21,7 @@ export default function Btn(props) {
 
   return (
     <div>
-        <input onClick={getData} className={styles.btn} type="button" value={props.text}/>
+        <input className={styles.btn} type="button" value={props.text}/>
     </div>
   )
 }
