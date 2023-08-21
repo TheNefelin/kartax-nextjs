e50f94e4d08db05a0aad4f66c04d389ce0d90a06
Francisco Carmona <flcarmonac@yahoo.com>
2023-08-21 01:21:40 -0400

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
+//css 
+import styles from "./page.module.css"
 //componentes
 import Phone from "@/components/Phone"
 
 export default function Home() {
   return (
     <main>
-      <section>
+      <section className={styles.section}>
         <div>
-          
+          <h1>Prueba</h1>
         </div>
         <Phone></Phone>
       </section>
