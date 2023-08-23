45b0f66efac0f23b35a15862e73574569e1654bb
Francisco Carmona <flcarmonac@yahoo.com>
2023-08-22 20:16:04 -0400

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,31 @@
+import Link from "next/link"
 //css 
 import styles from "./page.module.css"
 //componentes
 import Phone from "@/components/Phone"
+import Btn from "@/components/Btn"
 
 export default function Home() {
   return (
     <main>
       <section className={styles.section}>
-        <div>
-          <h1>Prueba</h1>
+        <div className={styles.title}>
+          <h1>Utiliza Nuestra App para Gestionar Pedidos desde la Mesa del Cliente</h1>
+          <h3>funcionalidades Gratuitas (gt)</h3>
+          <ul>
+            <li>Administrar 1 Negocio</li>
+            <li>Configuración Dashboard para Admin</li>
+            <li>Configuración Dashboard para Usuario</li>
+            <li>Mapeo de distribución de las mesas desde el Dashboard</li>
+            <li>QR por mesa que pueda realizar pedidos</li>
+            <li>Otros...</li>
+          </ul>
+          <Link
+            target="_blank"
+            href="https://kartax-express-production.up.railway.app/kartax/1"
+          >
+            <Btn text="Ver Demo"></Btn>
+          </Link>
         </div>
         <Phone></Phone>
       </section>
--- src/components/Phone.js
+++ src/components/Phone.js
@@ -4,12 +4,6 @@ import Link from "next/link"
 export default function Phone() {
   return (
     <section>
-      <Link
-        target="_blank"
-        href="https://kartax-express-production.up.railway.app/kartax/1">
-        Link Ver Demo
-      </Link>
-
       <div className={styles.phone}>
         <iframe className={styles.content} src="https://kartax-express-production.up.railway.app/kartax/1">
         </iframe>
