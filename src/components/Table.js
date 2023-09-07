f3b384603f6bd5ed607747e4110255dbf9c67a94
Francisco Carmona <flcarmonac@yahoo.com>
2023-09-07 17:05:14 -0300

    update

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,24 +2,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons"
 //css
 import styles from "@/css/Table.module.css"
-//fetching class
-import FetchingApi from "@/context/FetchingApi"
 
 export default function Table(props) {
-  const fetchingApi = new FetchingApi()
-
-  // Promise.all([
-  //   fetchingApi.get_business_byid_user(1).then(data => data.json()),
-  //   fetchingApi.get_items_grp_byid_business(1).then(data => data.json()),
-  //   fetchingApi.get_items_categ_byid_business(1).then(data => data.json()),
-  //   fetchingApi.get_items_byid_business(1).then(data => data.json()),
-  // ]).then(data => {
-  //   console.log(data[0])
-  //   console.log(data[1])
-  //   console.log(data[2])
-  //   console.log(data[3])
-  // })
-
   const tabla = props.tabla
   const columnas = Object.keys(tabla[0])
 
@@ -37,8 +21,8 @@ export default function Table(props) {
         <tbody>
           {tabla.map((filas, index) => (
             <tr key={index}>
-              {Object.values(filas).map(e => (
-                <td>{`${e}`}</td>
+              {Object.values(filas).map((e, eIndex) => (
+                <td key={eIndex}>{`${e}`}</td>
               ))}
               <td>
                 <FontAwesomeIcon icon={faPenToSquare} className={styles.icon}></FontAwesomeIcon>
